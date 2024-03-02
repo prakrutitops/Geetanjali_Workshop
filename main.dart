320327192c8e19efe8bd73528fa6590886abbc59
Prakruti_Tops <109515514+prakrutitops@users.noreply.github.com>
2024-03-02 12:37:31 +0530

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:project1/second.dart';
+
+void main()
+{
+  runApp(MaterialApp(home: MyApp(),debugShowCheckedModeBanner: false,));
+}
+
+class MyApp extends StatelessWidget
+{
+  @override
+  Widget build(BuildContext context)
+  {
+    return Scaffold
+      (
+        appBar: AppBar(title:Text("First App"),backgroundColor: Colors.blue,foregroundColor: Colors.deepOrange,
+          actions:
+          [
+
+              IconButton(onPressed: ()
+              {
+                  //pop -exit
+                  Navigator.pop(context);
+
+              }, icon: Icon(Icons.logout))
+
+          ],),
+        body: Center
+          (
+
+
+
+            child:Column(
+
+
+                children: [
+
+                  Text("Flutter",style:TextStyle(fontSize:50.00,color: Colors.deepPurple),),
+
+                  ElevatedButton(onPressed: ()
+                  {
+
+                      print("hello from xyz");
+
+                  }, child: Text("Click Me")),
+                  ElevatedButton(onPressed: ()
+                  {
+
+                    print("hello from xyz2");
+
+                  }, child: Text("Click Me2"))
+
+
+
+
+                ],
+
+            )
+
+
+
+
+
+          ),
+      floatingActionButton: FloatingActionButton(onPressed: ()
+      {
+
+          Navigator.push(context, MaterialPageRoute(builder: (context) => SecondPage()));
+
+
+      },child: Icon(Icons.add),),
+      );
+  }
+
+}
+
+
